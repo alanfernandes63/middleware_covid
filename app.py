e3387b612825cccb7b974124717c9123c02a646a
alan <alan.fernandes63@gmail.com>
2020-05-15 17:43:52 -0300

    modifica o arquivo app para resolver bug de deploy

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restplus import Api
 from flask_cors import CORS
 from resources.full_series import FullSeries
-
+import os
 
 
 app = Flask(__name__)
@@ -14,5 +14,7 @@ api.add_resource(FullSeries, '/api/data/')
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    #app.run(debug=False)
 
